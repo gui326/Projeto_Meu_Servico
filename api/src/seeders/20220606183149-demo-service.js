a9d7f7ae391d66ee629ccb74544231bc33bb97de
Gui326 <60348338+gui326@users.noreply.github.com>
2022-06-08 20:01:46 -0300

    realizado mais seedes serviços

--- api/src/seeders/20220606183149-demo-service.js
+++ api/src/seeders/20220606183149-demo-service.js
@@ -23,10 +23,28 @@ module.exports = {
       },
       {
         id: 3,
+        companyId: 1,
+        price: "100",
+        name: 'Avaliação da fiação elétrica',
+        description: 'Será realizado uma verificação na casa do cliente, afim de encontrar problemas na fiação',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        id: 4,
         companyId: 2,
         price: "29,99",
         name: 'Bolo Vulcão',
         description: 'Realização do bolo para o cliente',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        id: 5,
+        companyId: 2,
+        price: "35,99",
+        name: 'Bolo de Festa',
+        description: 'Bolo de Festa encomendado vendido por KG',
         createdAt: new Date(),
         updatedAt: new Date()
       },
