db761e26afeed99f89bd0acd8d482ff2a5917ecf
gui326 <guilherme.326@hotmail.com>
2022-06-17 02:03:16 -0300

    ajuste api

--- api/src/index.js
+++ api/src/index.js
@@ -8,27 +8,8 @@ const port = 3000;
 
 app.use(cors())
 
-app.use(function (req, res, next) {
-
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'https://projeto-meu-servico.vercel.app/');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
-});
-
 routes(app);
 
-app.listen(port, "localhost", () => console.log(`server is running in port ${port}`));
+app.listen(port, () => console.log(`server is running in port ${port}`));
 
 module.exports = app;
